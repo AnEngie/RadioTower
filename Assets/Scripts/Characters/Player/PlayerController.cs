fa78b4b84268a701f4648b3c8e1c464dca362295
Sed <sed.van.hesselingen@hva.nl>
2025-07-11 02:35:10 +0200

    Added Basic Player + Following Player Camera

---
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float moveSpeed;
+    public bool facingRight = true;
+    public LayerMask interactableLayer;
+
+    Vector2 moveInput;
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        set
+        {
+            _isMoving = value;
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+        }
+    }
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = walkSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SwitchFacingDirection(moveInput);
+    }
+
+    private void SwitchFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !facingRight)
+        {
+            facingRight = true;
+            transform.localScale *= new Vector2(-1, 1);
+        }
+        else if (moveInput.x < 0 && facingRight)
+        {
+            facingRight = false;
+            transform.localScale *= new Vector2(-1, 1);
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+            moveSpeed = runSpeed;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, interactableLayer) != null)
+        {
+            Debug.Log("AAAAAAAAH");
+        }
+    }
+}
